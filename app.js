40d2f3de6d8ceeb8457cc27a181c7544f3af06c9
Liam O'Gorman <logorman@cisco.com>
2013-01-17 16:46:03 -0800

    working server that serves presentations located as links from the ./presentations folder.

--- app.js
+++ app.js
@@ -3,33 +3,72 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+ var express = require('express')
+ , routes = require('./routes')
+ , user = require('./routes/user')
+ , http = require('http')
+ , path = require('path')
+ , dust = require('dustjs-linkedin')
+ , fs = require('fs');
 
-var app = express();
+ var app = express();
+ 
+ dust.optimizers.format = function(ctx, node) { return node };
 
-app.configure(function(){
+ app.configure(function(){
+  var compiled= dust.compile(
+    fs.readFileSync( path.join(__dirname, 'reveal.js', 'dust_index.html'), 'utf-8'),
+    "reveal");
+  dust.loadSource(compiled);
+
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  //app.set('views', __dirname + '/views');
+  //app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Add a handler for each presentation folder here. 
+  fs.readdir(path.join(__dirname, 'presentations'), function(err,files){
+   files.forEach(function(dir) {
+    var url = '/' + dir;
+    var dirPath = path.join(__dirname, 'presentations', dir, 'app');
+    
+    console.log("Found Pressie - configuring url=" + url + " dir=" + dirPath);
+    app.get(url, function(req, res){
+      fs.readFile(path.join(dirPath, "index.html"), "utf-8", function(err, data){
+        // Here we use the dust templating to 
+        // insert our slides and the title.
+        dust.render("reveal", {title: "present", slides: data}, function(err,out){
+          res.setHeader('Content-Type', 'text/html');
+          res.setHeader('Content-Length', out.length);
+          res.end(out);
+        });
+    });
+    });
+    app.use(url, express.static(dirPath));
+    app.use(url, express.static(path.join(__dirname, 'reveal.js')));
+  });
+ });
+
+
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+
+
+ //app.get('/', routes.index);
+ app.get('/users', user.list);
+
+
+
+
+ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
