e564bad80367c8c81069d806c2259191b80a6ed3
Liam O'Gorman <logorman@cisco.com>
2013-01-23 09:42:23 -0800

    Missed this in previous checkins - so should be part of previous checkin Use the presentations file. Big change is that the presentation should be slides.html now instead of index.html

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
  , http = require('http')
  , path = require('path')
  , dust = require('dustjs-linkedin')
+ , presentations= require('./presentations.js')
  , fs = require('fs');
 
  var app = express();
@@ -21,38 +22,27 @@
     "reveal");
   dust.loadSource(compiled);
 
-app.enable('strict routing');
+  app.enable('strict routing');
   app.set('port', process.env.PORT || 3000);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-
-  // Add a handler for each presentation folder here. 
-  fs.readdir(path.join(__dirname, 'presentations'), function(err,files){
-   files.forEach(function(dir) {
-    var url = '/' + dir;
-    var dirPath = path.join(__dirname, 'presentations', dir, 'app');
-
-    app.all(url, function(req, res) { res.redirect(url + '/'); });
-    
-    console.log("Found Pressie - configuring url=" + url + " dir=" + dirPath);
-    app.get(url +'/' , function(req, res){
-      fs.readFile(path.join(dirPath, "index.html"), "utf-8", function(err, data){
-        // Here we use the dust templating to 
-        // insert our slides and the title.
-        dust.render("reveal", {title: "present", slides: data}, function(err,out){
-          res.setHeader('Content-Type', 'text/html');
-          res.setHeader('Content-Length', out.length);
-          res.end(out);
-        });
+  // setup our route per presentation found
+  presentations.watch(path.join(__dirname, 'presentations'), function(presentation){
+    app.all(presentation.url, function(req, res) { res.redirect(presentation.url + '/'); });
+    app.get(presentation.url +'/' , function(req, res){
+      dust.render("reveal", presentation, function(err,out){
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Length', out.length);
+        res.end(out);
+      });
     });
-    });
-    app.use(url, express.static(dirPath));
-    app.use(url, express.static(path.join(__dirname, 'reveal.js')));
-  });
- });
+    app.use(presentation.url, express.static(presentation.path));
+    app.use(presentation.url, express.static(path.join(__dirname, 'reveal.js')));
+  })
+
 });
 
 app.configure('development', function(){
@@ -60,42 +50,42 @@ app.configure('development', function(){
 });
 
 var server = http.createServer(app);
-  var io =  require('socket.io').listen(server);
-  server.listen(app.get('port'), function(){
+var io =  require('socket.io').listen(server);
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
 
 
 io.sockets.on('connection', function(socket) {
-        console.log("socket connected");
-        socket.on('fragmenthidden', function(data) {
-                socket.broadcast.emit('fragmenthidden', data);
-                console.log("fragmenthidden - data=");
-                console.log(data);
-        });
+  console.log("socket connected");
+  socket.on('fragmenthidden', function(data) {
+    socket.broadcast.emit('fragmenthidden', data);
+    console.log("fragmenthidden - data=");
+    console.log(data);
+  });
 
-        socket.on('fragmentshown', function(data) {
-                socket.broadcast.emit('fragmentshown', data);
-                console.log("fragmentshown - data=");
-                console.log(data);
-        });
+  socket.on('fragmentshown', function(data) {
+    socket.broadcast.emit('fragmentshown', data);
+    console.log("fragmentshown - data=");
+    console.log(data);
+  });
 
-        socket.on('slidechanged', function(slideData) {
-                socket.broadcast.emit('slidedata', slideData);
-                console.log("slidechanged - data=");
-                console.log(slideData);
-        });
+  socket.on('slidechanged', function(slideData) {
+    socket.broadcast.emit('slidedata', slideData);
+    console.log("slidechanged - data=");
+    console.log(slideData);
+  });
 
-        socket.on('reload', function(slideData) {
-                socket.broadcast.emit('reload', slideData);
-                console.log("reload - data=");
-                console.log(slideData);
-        });
+  socket.on('reload', function(slideData) {
+    socket.broadcast.emit('reload', slideData);
+    console.log("reload - data=");
+    console.log(slideData);
+  });
 
-        socket.on('controller', function(command) {
-                socket.broadcast.emit('controller', command);
-                console.log("controller - data=");
-                console.log(command);
-        });
+  socket.on('controller', function(command) {
+    socket.broadcast.emit('controller', command);
+    console.log("controller - data=");
+    console.log(command);
+  });
 });
