a812f24ed7a0f420d201a0b96d7f828032d758ec
Liam O'Gorman <logorman@cisco.com>
2013-01-25 13:50:08 -0800

    Defaults for the reveal config must be set here so the template expansion doesn't cause problems on the client side.

--- presentations.js
+++ presentations.js
@@ -28,8 +28,53 @@ var presentations = exports = module.exports = {
 							_.extend(presentation, loadedconfig);
 							presentation.loadedConfig = loadedconfig;
 						}
-						_.defaults(presentation, {title:dir,controls:true, progress:true, history:true, center:true, transition:'default', theme:'default'}); 
-						
+						_.defaults(presentation, {
+							title:dir,
+											// Display controls in the bottom right corner
+							controls: true,
+
+							// Display a presentation progress bar
+							progress: true,
+
+							// Push each slide change to the browser history
+							history: false,
+
+							// Enable keyboard shortcuts for navigation
+							keyboard: true,
+
+							// Enable the slide overview mode
+							overview: true,
+
+							// Vertical centering of slides
+							center: true,
+
+							// Enables touch navigation on devices with touch input
+							touch: true,
+
+							// Loop the presentation
+							loop: false,
+
+							// Change the presentation direction to be RTL
+							rtl: false,
+
+							// Number of milliseconds between automatically proceeding to the
+							// next slide, disabled when set to 0, this value can be overwritten
+							// by using a data-autoslide attribute on your slides
+							autoSlide: 0,
+
+							// Enable slide navigation via mouse wheel
+							mouseWheel: false,
+
+							// Apply a 3D roll to links on hover
+							rollingLinks: true,
+							
+							// Transition effect, one of default/cube/page/concave/zoom/linear/none
+							transition:'default',
+
+							// theme
+							theme:'default'
+						});
+
 						fs.readFile(path.join(presentation.path, "slides.html"), "utf8", function(err, data) {
 							if(!err){
 								presentation.slides = data;
